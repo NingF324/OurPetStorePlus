ad41a87b3407138463c9acfb6dd1bc1fc81bb05b
NingF324 <2283451158@qq.com>
2024-03-22 08:28:56 +0800

    添加所有bean类

---
+++ src/main/java/org/ningf/ourpetstore/entity/Account.java
@@ -0,0 +1,9 @@
+package org.ningf.ourpetstore.entity;
+
+/**
+ * @description:
+ * @author: Lenovo
+ * @time: 2024/3/22 8:07
+ */
+public class Account {
+}
---
+++ src/main/java/org/ningf/ourpetstore/entity/LineItem.java
@@ -0,0 +1,9 @@
+package org.ningf.ourpetstore.entity;
+
+/**
+ * @description:
+ * @author: Lenovo
+ * @time: 2024/3/22 8:08
+ */
+public class LineItem {
+}
